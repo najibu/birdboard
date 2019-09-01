b4ae3aae48150b1fba855a7c41caa776d3594ac4
Najibu Nsubuga <ns.najibu@gmail.com>
2019-09-01 17:43:23 +0100

    project path

---
+++ tests/Unit/ProjectTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Project;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_has_a_path()
+    {
+        $project = factory(Project::class)->create();
+
+        $this->assertEquals('/projects/'. $project->id, $project->path());
+    }
+}

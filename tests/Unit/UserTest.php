8e994dabd32a0276ac949ef8aff8a845d3a07b45
Najibu Nsubuga <ns.najibu@gmail.com>
2019-09-07 16:03:16 +0100

    Project requires an owner

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function has_projects()
+    {
+        $user = factory(User::class)->create();
+
+        $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+}

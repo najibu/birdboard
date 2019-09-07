6bfc9a73885dc5e1c58357c8450381d22381d769
Najibu Nsubuga <ns.najibu@gmail.com>
2019-09-07 16:46:59 +0100

    User can view only their projects

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Project;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -17,5 +18,13 @@ class ProjectTest extends TestCase
         $project = factory(Project::class)->create();
 
         $this->assertEquals('/projects/'. $project->id, $project->path());
+    }
+
+    /** @test */
+    public function it_belongs_to_an_owner()
+    {
+        $project = factory(Project::class)->create();
+
+        $this->assertInstanceOf(User::class, $project->owner);
     }
 }
